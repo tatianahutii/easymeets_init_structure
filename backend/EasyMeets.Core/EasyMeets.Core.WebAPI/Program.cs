858b0c6dec76e6b4518d98ba38d99adbd8d6cf0c
Tati Hutii <tatiana.gutiy@gmail.com>
2022-08-04 01:06:35 +0300

    add enviroment variables

--- backend/EasyMeets.Core/EasyMeets.Core.WebAPI/Program.cs
+++ backend/EasyMeets.Core/EasyMeets.Core.WebAPI/Program.cs
@@ -3,6 +3,12 @@ using EasyMeets.Core.WebAPI.Extentions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", reloadOnChange: true, optional: true)
+    .AddEnvironmentVariables()
+    .Build();
 
 builder.Services.AddControllers();
 builder.Services.AddEasyMeetsCoreContext(builder.Configuration);
--- backend/EasyMeets.Notifier/EasyMeets.Notifier.WebAPI/Program.cs
+++ backend/EasyMeets.Notifier/EasyMeets.Notifier.WebAPI/Program.cs
@@ -2,14 +2,22 @@ using EasyMeets.Notifier.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", reloadOnChange: true, optional: true)
+    .AddEnvironmentVariables()
+    .Build();
 
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.WebHost.UseUrls("http://*:5070");
 
 var app = builder.Build();
 
@@ -24,6 +32,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
@@ -32,8 +43,6 @@ app.UseEndpoints(endpoints =>
 });
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
