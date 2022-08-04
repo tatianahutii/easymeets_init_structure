ef96cfc9ccf75fb8928899c848cb44b43fbccd96
Tati Hutii <38536017+tatianahutii@users.noreply.github.com>
2022-08-04 20:34:46 +0300

    Update main-routing.module.ts

--- frontend/src/app/modules/main/main-routing.module.ts
+++ frontend/src/app/modules/main/main-routing.module.ts
@@ -1,8 +1,9 @@
+import { MainComponent } from './main-page/main-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from '@shared/components/not-found/not-found.component';
 
-import { MainComponent } from './main-page/main-page.component';
+
 
 const routes: Routes = [
     {
